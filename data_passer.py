ebf064e85bd45e078b8d0cd0d667df1ddcceca40
Riley Bowyer <riley.d.bowyer@gmail.com>
2019-03-09 17:09:09 +1000

    Added A timestamp to packet

--- data_passer.py
+++ data_passer.py
@@ -1,5 +1,4 @@
 from google.transit import gtfs_realtime_pb2
-import time
 import requests
 
 lines = ['BDBN', 'BDBR', 'BDRW', 'BDVL', 'BNBD', 'BNBR', 'BNDB', 'BNFG', 'BNSH', 'BRBD', 'BRBN', 'BRCA', 'BRCL', 'BRDB', 'BRFG', 'BRGY', 'BRIP', 'BRNA', 'BRRP', 'BRRW', 'BRSH', 'BRSP', 'BRVL', 'CABR', 'CACL', 'CAIP', 'CARW', 'CASP', 'CLBR', 'CLDB', 'CLSH', 'DBBN', 'DBBR', 'DBCL', 'DBDB', 'FGBN', 'FGBR', 'GYBR', 'IPBR', 'IPCA', 'IPDB', 'IPFG', 'IPNA', 'IPRP', 'IPRW', 'NABR', 'NAIP', 'NASP', 'RBUS', 'RPBR', 'RPCL', 'RPIP', 'RPSP', 'RWBD', 'RWBR', 'RWCA', 'RWIP', 'RWNA', 'RWRP', 'SHBN', 'SHBR', 'SHCL', 'SHSP', 'SPBR', 'SPCA', 'SPDB', 'SPNA', 'SPRP', 'VLBD', 'VLBR', 'VLDB']
@@ -13,7 +12,7 @@ def get_train_data(chosen_lines):
         chosen_lines(list): List of Lines to get data for.
 
     Returns:
-        [current_time, (trip_id, route_id, stop_id, stopped, arrival_time, arrival_delay, departure_time, departure_delay, stop_sequence), ...]
+        [(time_stamp, trip_id, route_id, stop_id, stopped, arrival_time, arrival_delay, departure_time, departure_delay, stop_sequence), ...]
 
     """
     data = []
@@ -22,7 +21,6 @@ def get_train_data(chosen_lines):
 
     # Gather the Protobuffer
     feed = gtfs_realtime_pb2.FeedMessage()
-    current_time = round(time.time())
     r = requests.get("https://gtfsrt.api.translink.com.au/Feed/SEQ")
     feed.ParseFromString(r.content)
 
@@ -42,7 +40,6 @@ def get_train_data(chosen_lines):
             data.append(entity)
         
     
-    output.append(current_time)
     train_stopped = {}
     for entry in vehicle_data:
         trip_id = entry.vehicle.trip.trip_id
@@ -61,13 +58,14 @@ def get_train_data(chosen_lines):
         arrival_time = stop_info.arrival.time
         departure_time = stop_info.departure.time
         stop_sequence = stop_info.stop_sequence
+        time_stamp = entity.timestamp
 
         departure_delay = stop_info.departure.delay
         arrival_delay = stop_info.arrival.delay
 
         stopped = train_stopped.get(trip_id)
         
-        packet = (trip_id, route_id, stop_id, stopped, arrival_time, arrival_delay, departure_time, departure_delay, stop_sequence)
+        packet = (time_stamp, trip_id, route_id, stop_id, stopped, arrival_time, arrival_delay, departure_time, departure_delay, stop_sequence)
         output.append(packet)
 
     print(output)
